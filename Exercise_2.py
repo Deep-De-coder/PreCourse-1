1a0933d0d578dfdb3dbdca9734ef7d4c2fe9b82a
Deep Shahane <61946392+Deep-De-coder@users.noreply.github.com>
2025-01-31 19:15:05 -0500

    Precourse1

--- Exercise_2.py
+++ Exercise_2.py
@@ -6,10 +6,19 @@ class Node:
  
 class Stack:
     def __init__(self):
-        
+        self.top = None
+
     def push(self, data):
-        
+        new_node = Node(data)
+        new_node.next = self.top
+        self.top = new_node
+
     def pop(self):
+        if self.top is None:
+            return None
+        popped_data = self.top.data
+        self.top = self.top.next
+        return popped_data
         
 a_stack = Stack()
 while True:
